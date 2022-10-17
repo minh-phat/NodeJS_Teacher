4f46c5b2fcdaa56208089fc68df2da03ac80bc74
minh-phat <nguyenleminhkhang0608@gmail.com>
2022-10-17 14:00:37 +0700

    1

--- controller/productController.js
+++ controller/productController.js
@@ -5,6 +5,11 @@ const fs = require("fs");
 ////// - Model
 const Product = require("../model/Product");
 
+// Upload - https://www.npmjs.com/package/multer 
+
+const multer = require('multer');
+const upload = multer({dest: 'public/img'});
+
 //-------------------------------------------
 router.get( "/" , productHome);
 async function productHome(yeucau, trave) {
@@ -21,16 +26,31 @@ router.get( "/create" , (yeucau, trave) => {
     trave.render("insertProduct");
 });
 
-router.post( "/create" , (yeucau, trave) => {
+router.post( "/create" , upload.single("myFile"), (yeucau, trave, ketiep) => {
     console.log("\n BODY: ", yeucau.body);
     console.log("\n Params: ", yeucau.params);
     console.log("\n Query: ", yeucau.query);
-    
+    console.log("\n File: ", yeucau.file);
+
+    yeucau.body.ImageLink = "/images/" + yeucau.file.filename;
     oneproduct = new Product(yeucau.body);
     oneproduct.save();
-    
-    trave.render("insertProduct");
-    //trave.render("showProduct",  {sanpham: yeucau.body});
+    trave.render("showProduct",  {sanpham: yeucau.body});
+});
+
+router.get( "/view/:MaSP" , async (yeucau, trave) => {
+    console.log("\n BODY: ", yeucau.body);
+    console.log("\n Params: ", yeucau.params);
+    console.log("\n Query: ", yeucau.query);
+
+    try {
+        let sp = await Product.findOne({ MaSP: yeucau.params.MaSP });
+        console.log(sp);
+        trave.render("oneproduct", {sanpham: sp});
+    } catch (error) {
+        console.log(error);
+    }
+
 });
 
 router.get( "/giadung" , (yeucau, trave) => {
